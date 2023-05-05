a410c617f2d2af2aea8c67bab4e4adaa8704cca0
kaiocesarnp <kaiocesarnp@gmail.com>
2023-05-05 16:50:02 -0300

    playmusic

--- music.py
+++ music.py
@@ -20,7 +20,13 @@ pygame.quit()
 
 # import pygame: Esta linha importa a biblioteca Pygame, que é uma biblioteca de jogos em Python que também pode ser usada para trabalhar com áudio e vídeo.
 
-# 
+# pygame.init(): Esta linha inicializa a biblioteca Pygame. É importante chamar esta função antes de usar qualquer outra função em Pygame.
+
+# pygame.mixer.music.load("tovaritch_mode_avion.mp3"): Esta linha carrega o arquivo de música "tovaritch_mode_avion.mp3" na memória. A função "load()" da classe "pygame.mixer.music" é usada para carregar o arquivo de música.
+
+# pygame.mixer.music.play(): Esta linha inicia a reprodução da música carregada anteriormente. A função "play()" da classe "pygame.mixer.music" é usada para começar a tocar a música.
+
+
 
 
 
